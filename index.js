bfc40edb5ccfc01861f3c608ba4829642522bbba
Tristan <tristanw747@gmail.com>
2023-02-04 10:51:46 -0800

    updates

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 import fetch from 'node-fetch';
-import dotenv from "dotenv";
 import https from 'https';
-dotenv.config();
+import dotenv from "dotenv";
+dotenv.config()
 
-async function mainFunction() {
-  await fetch(process.env.FOLEY_PREP_INTERVIEW_URL,
+function mainFunction() {
+  fetch(process.env.FOLEY_PREP_INTERVIEW_URL,
     { agent: new https.Agent({ rejectUnauthorized: false }) })
     .then(res => res.json())
     .then(json => {
-      let studentAnswers = json.users;
-      let testAnswers = json.tests;
-      let finalResult = []
-      if (testAnswers[0].name === "SAT") { gradingFunction(testAnswers[0], studentAnswers, finalResult) }
-      if (testAnswers[1].name === "ACT") { gradingFunction(testAnswers[1], studentAnswers, finalResult) }
-      console.log(finalResult)
-      return finalResult
+      const studentAnswers = json.users;
+      const testAnswers = json.tests;
+      const finalGradeResults = []
+      if (testAnswers[0].name === "SAT") { gradingFunction(testAnswers[0], studentAnswers, finalGradeResults) }
+      if (testAnswers[1].name === "ACT") { gradingFunction(testAnswers[1], studentAnswers, finalGradeResults) }
+      console.log(finalGradeResults)
+      return finalGradeResults
     })
+    .catch((err) => console.log(err))
 }
 
-function gradingFunction(testType, studentAnswers, finalResult) {
+function gradingFunction(testType, studentAnswers, finalGradeResults) {
   studentAnswers.forEach(singleStudent => {
     if (singleStudent.test === testType.name) {
       let studentCorrectAnswers = [];
@@ -27,12 +28,13 @@ function gradingFunction(testType, studentAnswers, finalResult) {
           studentCorrectAnswers.push(true)
         }
       }
-      finalResult.push({
+      finalGradeResults.push({
         name: singleStudent.name,
         grade: `${gradingMethod(studentCorrectAnswers)}%`
       })
     }
   })
+
   function gradingMethod(studentCorrectAnswers) {
     return (studentCorrectAnswers.length / testType.answers.length) * 100
   }
